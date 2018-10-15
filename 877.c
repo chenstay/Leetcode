ab5d7650e894175cc3f5d389b41d7c278479125a
chenstay <34984302+chenstay@users.noreply.github.com>
2018-10-15 22:02:49 +0800

    Create 877.c

---
+++ 877.c
@@ -0,0 +1,8 @@
+//great question!
+//ref https://leetcode.com/problems/stone-game/discuss/154610/C%2B%2BJavaPython-DP-or-Just-return-true
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
