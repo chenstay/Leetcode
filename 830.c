bf2fd7fc2303aea46a35306b1d117a6104623cb2
chenstay <34984302+chenstay@users.noreply.github.com>
2018-10-17 22:11:52 +0800

    Update 830.c

--- 830.c
+++ 830.c
@@ -5,10 +5,8 @@ public:
         vector<int> temp;
         vector<vector<int>> res;
         int left=0;
-        bool a=true;
         for(int i=0;i<n;i++){
             if(S[i]!=S[i+1]||i==n-1){
-                a=false;
                 if((i-left)>=2){
                     temp.push_back(left);
                     temp.push_back(i);
