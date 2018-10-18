ad84427bb91cbe30b247937061968d95fc5f2f1d
chenstay <34984302+chenstay@users.noreply.github.com>
2018-10-18 18:06:17 +0800

    Update 852.c

--- 852.c
+++ 852.c
@@ -6,5 +6,21 @@ public:
             if(A[i]>A[i-1]&&A[i]>A[i+1])return i;
         }
         return 0;
+        
+        //solution 2
+        //vector<int>::iterator max_iter = max_element(A.begin(), A.end());
+        //return distance(A.begin(), max_iter);
+        
+        //solution 3
+        /*int n=A.size();
+        int left=0;
+        int right=n-1;
+        while(left<right){
+            int mid=(left+right)/2;
+            if(A[mid]>A[mid+1])right=mid;
+            else left=mid+1;
+        }
+        return right;*/
+        //right==left
     }
 };
