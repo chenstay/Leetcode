feddc18dd69d80cf1a032db191d9e95abaa49531
chenstay <34984302+chenstay@users.noreply.github.com>
2018-10-26 12:57:55 +0800

    Update 153.c

--- 153.c
+++ 153.c
@@ -8,3 +8,20 @@ public:
         return nums[0];
     }
 };
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int n=nums.size();
+        int left=0;
+        int right=n-1;
+        vector<int> a=nums;
+        sort(a.begin(),a.end());
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]>a[mid])left=mid+1;
+            else right=mid;
+        }
+        return nums[left];
+    }
+};
