7fb49c6e9a95d4b2a423842867c671e5a2eb2fde
chenstay <34984302+chenstay@users.noreply.github.com>
2018-11-04 22:32:17 +0800

    Create 110.c

---
+++ 110.c
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if(!root)return true;
+        return abs(dep(root->left)-dep(root->right))<=1;
+    }
+private:
+    int dep(TreeNode* root){
+        if(!root)return 0;
+        return max(dep(root->left),dep(root->right))+1;
+    }
+};
