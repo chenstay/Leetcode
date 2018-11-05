e8524f8fbf2348ffe9ee0de06f4fbb353ac2fdbd
chenstay <34984302+chenstay@users.noreply.github.com>
2018-11-05 21:09:54 +0800

    Update 935.c

--- 935.c
+++ 935.c
@@ -32,3 +32,31 @@ private:
         return sum;
     }
 };
+
+class Solution {
+public:
+    int knightDialer(int N) {
+        //f[i][j] means reach number i by step j+1
+        long long int f[10][5000];
+        memset(f, -1, sizeof(f));
+        const long long int MOD = 1e9+7;
+        long long int ans = 0;
+        for (int i = 0; i < 10; i++) f[i][0] = 1;
+        for (int i = 1; i < N; i++) {
+            f[0][i] = (f[4][i-1] + f[6][i-1]) % MOD;
+            f[1][i] = (f[6][i-1] + f[8][i-1]) % MOD;
+            f[2][i] = (f[7][i-1] + f[9][i-1]) % MOD;
+            f[3][i] = (f[4][i-1] + f[8][i-1]) % MOD;
+            f[4][i] = (f[0][i-1] + f[3][i-1] + f[9][i-1]) % MOD;
+            f[5][i] = 0;
+            f[6][i] = (f[0][i-1] + f[1][i-1] + f[7][i-1]) % MOD;
+            f[7][i] = (f[2][i-1] + f[6][i-1]) % MOD;
+            f[8][i] = (f[1][i-1] + f[3][i-1]) % MOD;
+            f[9][i] = (f[2][i-1] + f[4][i-1]) % MOD;
+        }
+        for (int i = 0; i < 10; i++)
+            ans = (ans + f[i][N-1]) % MOD;
+        return ans;
+    }
+};
+//https://zhanghuimeng.github.io/post/leetcode-935-knight-dialer/
