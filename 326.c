fbcaed9785b67448fd1add793858ff85e99b39cb
chenstay <34984302+chenstay@users.noreply.github.com>
2018-11-07 10:59:42 +0800

    Create 326.c

---
+++ 326.c
@@ -0,0 +1,11 @@
+//342 Power of Four
+//231 Power of Two
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        while(n%3==0&&n>1){
+            n/=3;
+        }
+        return n==1;
+    }
+};
