4789026149ec0cc22cb4bc44ebe4d684adaef2d3
chenstay <34984302+chenstay@users.noreply.github.com>
2018-11-07 09:50:09 +0800

    Update 142.c

--- 142.c
+++ 142.c
@@ -26,3 +26,33 @@ public:
         return fast;
     }
 };
+
+//base 141
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if(!head)return NULL;
+        ListNode *fast=head;
+        ListNode *slow=head;
+        while(fast&&fast->next){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)break;
+        }
+        if(!fast||!fast->next)return NULL;
+        slow=head;
+        while(slow!=fast){
+            slow=slow->next;
+            fast=fast->next;
+        }
+        return fast;
+    }
+};
