c14863554b51543a3df4ab061151a6d1cb19941d
chenstay <34984302+chenstay@users.noreply.github.com>
2018-11-07 08:50:31 +0800

    Update 067.c

--- 067.c
+++ 067.c
@@ -33,3 +33,18 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string res="";
+        int i=a.size()-1;int j=b.size()-1;int c=0;
+        while(i>=0||j>=0||c==1){
+            c+=i>=0?a[i--]-'0':0;
+            c+=j>=0?b[j--]-'0':0;
+            res=char(c%2+'0')+res;
+            c/=2;
+        }
+        return res;
+    }
+};
