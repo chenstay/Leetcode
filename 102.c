adb4a660e864f1b0ca5f43593f2db6ffdb5e2f4e
chenstay <34984302+chenstay@users.noreply.github.com>
2018-11-17 20:00:39 +0800

    Create 102.c

---
+++ 102.c
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        if(!root)return {};
+        queue<TreeNode*> q;
+        q.push(root);
+        vector<vector<int>> res;
+        while(!q.empty()){
+            int n=q.size();
+            vector<int> tmp;
+            for(int i=0;i<n;i++){
+                TreeNode* t=q.front();
+                tmp.push_back(t->val);
+                q.pop();
+                if (t->left) q.push(t->left);
+                if (t->right) q.push(t->right);
+            }
+            res.push_back(tmp);
+        }
+        return res;
+    }
+};
