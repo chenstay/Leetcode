f7b14078f0039c79b61895cb9ed12958b139161d
chenstay <34984302+chenstay@users.noreply.github.com>
2018-11-21 21:48:23 +0800

    Update 083.c

--- 083.c
+++ 083.c
@@ -23,3 +23,28 @@ public:
         return res&&res->next?res->next:res;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if(!head||!head->next)return head; 
+        ListNode* cur=head;
+        while(cur&&cur->next){
+            if(cur->val==cur->next->val){
+                ListNode* tmp=cur->next;
+                cur->next=cur->next->next;
+                delete tmp;
+                }
+            else cur=cur->next;
+        }
+        return head;
+    }
+};
