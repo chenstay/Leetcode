80b42a29305eb1dd90e60de5290b3dc81b14064d
chenstay <34984302+chenstay@users.noreply.github.com>
2018-11-21 20:32:54 +0800

    Update 206.c

--- 206.c
+++ 206.c
@@ -22,3 +22,28 @@ public:
 };
 //迭代法
 //https://blog.csdn.net/qq_17550379/article/details/80647926?utm_source=blogkpcl5
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* pNode=head;
+        ListNode* res=NULL;
+        ListNode* pre=NULL;
+        while(pNode){
+            if(!pNode->next){pNode->next=pre;return pNode;}
+            ListNode* pNext=pNode->next;
+            pNode->next=pre;
+            pre=pNode;
+            pNode=pNext;
+        }
+        return NULL;
+    }
+};
