c58cb9fdeeb7cec62570faa8840bd0c8bf2450b4
chenstay <34984302+chenstay@users.noreply.github.com>
2018-11-21 19:25:55 +0800

    Update 021.c

--- 021.c
+++ 021.c
@@ -26,3 +26,27 @@ public:
         return res->next;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {;
+        while(!l1)return l2;
+        while(!l2)return l1;
+        if(l1->val>l2->val){
+            l2->next=mergeTwoLists(l1,l2->next);
+            return l2;
+        }
+        else {
+            l1->next=mergeTwoLists(l1->next,l2);
+            return l1;
+        }
+    }
+};
