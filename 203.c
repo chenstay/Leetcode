89fad30f8737d2ef5a1c3b3f6cebfd02fc7f70b1
chenstay <34984302+chenstay@users.noreply.github.com>
2018-11-23 16:38:53 +0800

    Update 203.c

--- 203.c
+++ 203.c
@@ -24,3 +24,21 @@ public:
         return pre->next;
     }
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        if(!head)return head;
+        head->next=removeElements(head->next,val);
+        return head->val==val?head->next:head;
+    }
+};
