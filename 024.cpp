a6b841c633c712d3824161d771610e6bc45ef9dd
chenstay <34984302+chenstay@users.noreply.github.com>
2018-12-03 22:14:47 +0800

    Update 024.cpp

--- 024.cpp
+++ 024.cpp
@@ -30,3 +30,14 @@ public:
         return res;
     }
 };
+
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head||!head->next)return head;
+        ListNode* next=head->next;
+        head->next=swapPairs(next->next);
+        next->next=head;
+        return next;
+    }
+};
