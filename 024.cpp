d09f0cde5c1518384f2bdb89d486ae44d6acb029
chenstay <34984302+chenstay@users.noreply.github.com>
2018-12-03 22:11:18 +0800

    Create 024.cpp

---
+++ 024.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapPairs(ListNode* head) {
+        if(!head||!head->next)return head;
+        ListNode* tmp=head;
+        //initial
+        ListNode* res=new ListNode(tmp->next->val);
+        ListNode* cur=res;
+        cur->next=new ListNode(tmp->val);
+        cur=cur->next;
+        tmp=tmp->next;
+        //reverse number
+        while(tmp&&tmp->next&&tmp->next->next){
+            cur->next=new ListNode(tmp->next->next->val);
+            cur=cur->next;
+            cur->next=new ListNode(tmp->next->val);
+            cur=cur->next;
+            tmp=tmp->next->next;
+        }
+        //the last odd one
+        if(tmp->next)cur->next=new ListNode(tmp->next->val);
+        return res;
+    }
+};
