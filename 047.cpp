1c5c6d340686cb8fa0895bd2ea7f74050237b7ea
chenstay <34984302+chenstay@users.noreply.github.com>
2018-12-06 10:24:10 +0800

    Update 047.cpp

--- 047.cpp
+++ 047.cpp
@@ -32,3 +32,27 @@ public:
     }
 };
 
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> res;
+        vector<int> tmp;
+        vector<int> visited(nums.size(),0);
+        permuteDFS(res,nums,tmp,0,visited);
+        return res;
+    }
+    void permuteDFS(vector<vector<int>>& res,vector<int> &num,vector<int>& tmp,int index,vector<int>& visited){
+        if(index>=num.size())res.push_back(tmp);
+        for(int i=0;i<num.size();i++){//从0开始
+            if(visited[i]==0){
+                if(i>0&&num[i]==num[i-1]&&visited[i-1]==0)continue;//和前面的数相等且前面的数并未曾使用过
+                visited[i]=1;
+                tmp.push_back(num[i]);
+                permuteDFS(res,num,tmp,index+1,visited);
+                tmp.pop_back();
+                visited[i]=0;
+            }
+        }
+    }
+};
