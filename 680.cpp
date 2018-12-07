cf2d65ebf8e4cae413d7e46a36d886e314e5e478
chenstay <34984302+chenstay@users.noreply.github.com>
2018-12-07 11:43:21 +0800

    Create 680.cpp

---
+++ 680.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool validPalindrome(string s) {
+        int i=0,j=s.size()-1;
+        while(i<j){
+            if(s[i]!=s[j])return isValid(s,i+1,j)||isValid(s,i,j-1);
+            i++;j--;
+        }
+        return true;
+    }
+    bool isValid(string s,int i,int j){
+        while(i<j){
+            if(s[i]!=s[j])return false;
+            i++;j--;
+        }
+        return true;
+    }
