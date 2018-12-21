2773a2030a3eb7b821d5a6aafbf4dc3abf9875ba
chenstay <34984302+chenstay@users.noreply.github.com>
2018-12-21 20:18:59 +0800

    Update 145.cpp

--- 145.cpp
+++ 145.cpp
@@ -18,3 +18,30 @@ public:
         return res;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* t=q.top();q.pop();
+            if(!t)continue;//empty
+            res.push_back(t->val);
+            q.push(t->left);
+            q.push(t->right);
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
