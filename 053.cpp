0378c9bf5cfedf36649a0a4a81badc13bfa12c03
chenstay <34984302+chenstay@users.noreply.github.com>
2019-01-14 22:09:26 +0800

    Update 053.cpp

--- 053.cpp
+++ 053.cpp
@@ -11,3 +11,19 @@ public:
         return *max_element(dp.begin(),dp.end());
     }
 };
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n=nums.size();
+        if(n==0)return 0;
+        vector<int> dp(n,0);
+        dp[0]=nums[0];
+        int res=nums[0];
+        for(int i=1;i<n;i++){
+            dp[i]=max(dp[i-1]+nums[i],nums[i]);
+            res=max(res,dp[i]);
+        }
+        return res;
+    }
+};
