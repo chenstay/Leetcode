85d32e419915c03da9b81568489ee8d08d4ea938
chenstay <34984302+chenstay@users.noreply.github.com>
2019-03-20 11:37:39 +0800

    Update 199.cpp

--- 199.cpp
+++ 199.cpp
@@ -23,3 +23,33 @@ public:
         return res;
     }
 };
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        vector<int> res;
+        if(!root)return res;
+        while(!q.empty()){
+            res.push_back(q.back()->val);
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                TreeNode* tmp=q.front();
+                q.pop();
+                if(tmp->left)q.push(tmp->left);
+                if(tmp->right)q.push(tmp->right);
+            }            
+        }
+        return res;
+        
+    }
+};
