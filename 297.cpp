464925dd19e8f9eeb4387968535d2c547ee69eb0
chenstay <34984302+chenstay@users.noreply.github.com>
2019-03-22 10:57:41 +0800

    Update 297.cpp

--- 297.cpp
+++ 297.cpp
@@ -48,3 +48,65 @@ private:
 // Codec codec;
 // codec.deserialize(codec.serialize(root));
 //same to 449.cpp
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        if(!root)return "";
+        ostringstream out;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* tmp=q.front();
+            q.pop();
+            if(tmp){
+                out<<tmp->val<<' ';
+                q.push(tmp->left);
+                q.push(tmp->right);
+            }
+            else out<<"# ";
+        }
+        return out.str();
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        if (data.empty()) return NULL;
+        istringstream is(data);
+        queue<TreeNode*> q;
+        string val = "";
+        is >> val;
+        TreeNode *res = new TreeNode(stoi(val)), *cur = res;
+        q.push(cur);
+        while (!q.empty()) {
+            TreeNode *t = q.front(); q.pop();
+            if (!(is >> val)) break;
+            if (val != "#") {
+                cur = new TreeNode(stoi(val));
+                q.push(cur);
+                t->left = cur;
+            }
+            if (!(is >> val)) break;
+            if (val != "#") {
+                cur = new TreeNode(stoi(val));
+                q.push(cur);
+                t->right = cur;
+            }
+        }
+        return res;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec;
+// codec.deserialize(codec.serialize(root));
