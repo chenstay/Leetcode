e35c3618f1a3bc86ba5aa8b27444469cec2a541b
chenstay <34984302+chenstay@users.noreply.github.com>
2019-03-27 10:49:38 +0800

    Update 729.cpp

--- 729.cpp
+++ 729.cpp
@@ -23,3 +23,25 @@ private:
  * MyCalendar* obj = new MyCalendar();
  * bool param_1 = obj->book(start,end);
  */
+class MyCalendar {
+public:
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+        auto it=cal.lower_bound(start);//it.first>=start
+        if(it!=cal.end()&&it->first<end)return false;
+        //prev(it).first<start
+        if(it!=cal.begin()&&start<prev(it)->second)return false;
+        return true;
+    }
+private:
+    map<int,int> cal;
+};
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
