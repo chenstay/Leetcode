99f87e83035d5e6836a25161e62b83a567bc2651
chenstay <34984302+chenstay@users.noreply.github.com>
2019-04-20 10:26:34 +0800

    Update 032.cpp

--- 032.cpp
+++ 032.cpp
@@ -18,3 +18,22 @@ public:
         return res;
     }
 };
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        int n=s.size();
+        vector<int> dp(n+1,0);
+        int left=0;
+        int res=0;
+        for(int i=0;i<n;i++){
+            if(s[i]=='(')left++;
+            else if(s[i]==')'&&left>0){
+                dp[i+1]=dp[i]+2;
+                dp[i+1]+=dp[i+1-dp[i+1]];
+                left--;
+            }
+            res=max(res,dp[i+1]);
+        }
+        return res;
+    }
+};
