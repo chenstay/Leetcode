7801b4710f662eabd3fd81da5f9c5e56f8a88455
chenstay <34984302+chenstay@users.noreply.github.com>
2019-04-20 09:23:40 +0800

    Update 055.cpp

--- 055.cpp
+++ 055.cpp
@@ -22,3 +22,16 @@ public:
         return reach>=n-1;
     }
 };
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int reach=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(reach<i)return false;
+            if(reach>=n-1)return true;
+            reach=max(reach,nums[i]+i);
+        }
+        return nums[n-1]>0;
+    }
+};
