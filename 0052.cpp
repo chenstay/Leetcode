9470660f2ef632921421f0c4c5dd0a8dbce1ad63
chenstay <34984302+chenstay@users.noreply.github.com>
2019-09-24 11:32:44 +0800

    Create 0052.cpp

---
+++ 0052.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int totalNQueens(int n) {
+        int res=0;
+        vector<int> pos(n,-1);
+        DFS(pos,0,res);
+        return res;
+    }
+    void DFS(vector<int> &pos,int row,int &res){
+        int n=pos.size();
+        if(row==n){
+            res++;
+        }
+        else{
+            for(int col=0;col<n;col++){
+                if(isValid(pos,row,col)){
+                    pos[row]=col;
+                    DFS(pos,row+1,res);
+                    pos[row]=-1;
+                }
+            }
+        }
+    }
+    bool isValid(vector<int> &pos,int row,int col){
+        for(int i=0;i<row;i++){
+            //(row,col)与(i,pos[i])比较
+            if(pos[i]==col||abs(row-i)==abs(col-pos[i]))return false;
+        }
+        return true;
+    }
+};
