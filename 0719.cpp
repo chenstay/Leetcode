19922e8aef7c0e2661df4a7124a480a7727eff72
chenstay <34984302+chenstay@users.noreply.github.com>
2019-09-27 17:33:23 +0800

    Create 0719.cpp

---
+++ 0719.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int smallestDistancePair(vector<int>& nums, int k) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size(), left = 0, right = nums.back() - nums[0];
+        while (left < right) {
+            int mid=left+(right-left)/2;
+            int cnt=0;
+            int start=0;
+            for(int i=0;i<n;i++){
+                while(start<n&&nums[i]-nums[start]>mid)start++;
+                cnt+=i-start;
+            }
+            if(cnt<k)left=mid+1;
+            else right=mid;
+        }
+        return right;
+    }
+};
