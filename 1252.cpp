7718892aaccbf0ae6b6cd192cb8b81b667c20fd2
chenstay <34984302+chenstay@users.noreply.github.com>
2019-11-14 10:05:53 +0800

    Update 1252.cpp

--- 1252.cpp
+++ 1252.cpp
@@ -16,3 +16,23 @@ public:
         return res;
     }
 };
+class Solution {
+public:
+    int oddCells(int n, int m, vector<vector<int>>& indices) {
+        int res1=0;
+        int res2=0;
+        int myRow[n]={0};
+        int myCol[m]={0};
+        for(int i=0;i<indices.size();i++){
+            myRow[indices[i][0]]++;
+            myCol[indices[i][1]]++;  
+        }
+        for(int i=0;i<n;i++){
+            if(myRow[i]%2==1)res1++;
+        }
+        for(int i=0;i<m;i++){
+            if(myCol[i]%2==1)res2++;
+        }
+        return res1*m+res2*n-res1*res2*2;
+    }
+};
