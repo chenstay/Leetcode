2e746862188b25437ab4f25d4f4726b0eedf214e
chenstay <34984302+chenstay@users.noreply.github.com>
2019-12-18 15:36:03 +0800

    Create 0738.cpp

---
+++ 0738.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int monotoneIncreasingDigits(int N) {
+        string str=to_string(N);
+        int n=str.size();
+        int j=n;
+        for(int i=n-1;i>0;i--){
+            if(str[i]>=str[i-1])continue;
+            else{
+                j=i;
+                str[i-1]--;
+            }
+        }
+        for(int i=j;i<n;i++){
+            str[i]='9';
+        }
+        return stoi(str);
+    }
+};
